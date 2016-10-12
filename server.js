246fece4b0f1f24e3a07f378c9e2698dfd9e1c8c
afanasievov <afanasievov@gmail.com>
2016-10-12 23:40:50 +0300

    Added uploading of images.

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 'use strict';
 
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
+    let postData = '';
     const pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received.`);
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    const content = route(handle, pathname);
-    response.write(content);
-    response.end();
+    request.setEncoding('utf8');
+
+    request.addListener('data',postDataChunk => {
+      postData += postDataChunk;
+      console.log(`Received POST data chunk '${postDataChunk}'.`);
+    });
+
+    request.addListener('end', () => route(handle, pathname, response, postData));
+
   }
 
   http.createServer(onRequest).listen(8888);
